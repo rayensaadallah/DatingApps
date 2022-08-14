223034fdb4a482d0c5555beda471083bba432e18
rayen <saadallah.rayen@gmil.com>
2022-08-14 02:54:58 +0200

    dtos done but still facing 404 error on register

--- API/Controllers/AccountControllers.cs
+++ API/Controllers/AccountControllers.cs
@@ -5,8 +5,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,20 +19,26 @@ namespace API.Controllers
         {
             _context = context;
         }
-        [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username,string password)
+        [HttpPost("register")]// 404 erro 
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
         {
+            if(await UserExist(registerDto.Username)) return BadRequest("Username is taken");
+
             using var hmac = new HMACSHA512();
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
 
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+        private async Task<bool> UserExist(string Username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == Username.ToLower());
         }
     }
 }
