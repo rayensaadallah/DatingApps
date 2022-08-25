0d999488c5bff76eb0f52d5e626e22b8f58a28c4
rayen <saadallah.rayen@gmil.com>
2022-08-26 00:20:24 +0200

    end section 6 :D

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,7 @@ export class NavComponent implements OnInit {
   model:any={}
 
 
-  constructor(public accountServices:AccountService) { }
+  constructor(public accountServices:AccountService,private router:Router,private toastr:ToastrService) { }
 
   ngOnInit(): void {
   
@@ -20,13 +22,16 @@ export class NavComponent implements OnInit {
 
 login(){
   this.accountServices.login(this.model).subscribe(Response => {
-    console.log(Response);
+   this.router.navigateByUrl('/members')
   },error =>{
     console.log(error);
+    this.toastr.error(error.error);
   }
   )}
+
   logout(){
     this.accountServices.logout();
+    this.router.navigateByUrl('/');
 
   }
  
